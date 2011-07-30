9970c95df7c7cfe888b88dc118cf5a5b3f2e1a04
Hugo Amorim <h.amorim.lyra@gmail.com>
2011-07-30 15:57:52 -0300

    Adicionando Estrutura.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clubedalista::Application.config.secret_token = '75893972359e1dd81cbeb63a6c42850c478e89063a9887eefb177d0abab5e67cf91d610146d4e06ae21012a08ce7251dc7b0851b24886969b3ec45612e8431a3'
